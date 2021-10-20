09d0e7a466de40da6af804087b8328cfc284db6a
Ctrl-Mota <lucas.d.mota@gmail.com>
2021-10-19 23:42:01 -0300

    use cachedResources, implament usereducer and use context for all components

--- App.js
+++ App.js
@@ -1,3 +1,28 @@
-import App from './src';
+import React, { useReducer } from 'react';
+import { Provider as ProviderPaper } from 'react-native-paper'
+import { NavigationContainer } from '@react-navigation/native';
+import { Main } from './src/navigation/Main';
+import useCustomTheme from './src/util/hooks/useCustomTheme';
+import useCachedResources from './src/util/hooks/useCachedResources';
+import { coinReducer, DispatchContext, initialState, StateContext } from './src/util/redux/reducer';
 
-export default App;
+export default function App() {
+    const isLoadingComplete = useCachedResources();
+    const [state, dispatch] = useReducer(coinReducer, initialState);
+    const theme = useCustomTheme();
+    if (!isLoadingComplete) {
+        return null;
+    } else {
+        return (
+            <DispatchContext.Provider value={dispatch}>
+                <StateContext.Provider value={state}>
+                    <ProviderPaper theme={theme}>
+                        <NavigationContainer>
+                            <Main />
+                        </NavigationContainer>
+                    </ProviderPaper>
+                </StateContext.Provider>
+            </DispatchContext.Provider>
+        );
+    }
+}
